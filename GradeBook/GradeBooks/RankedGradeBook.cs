828c265efe0e72b1cc8f8f2f148813ebfa2d389d
taylortackett-basised <41164433+taylortackett-basised@users.noreply.github.com>
2018-07-12 15:16:25 -0700

    minor change

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name)
+        RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
         }
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class StandardGradeBook : BaseGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name) : base(name)                  // adding : base(name) after constructor declaration makes the program execute code in both classes (parent/child)
+        StandardGradeBook(string name) : base(name)                  // adding : base(name) after constructor declaration makes the program execute code in both classes (parent/child)
         {
             Type = Enums.GradeBookType.Standard;
         }
