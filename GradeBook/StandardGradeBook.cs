c47cd27edd937a568fbe31ce0fc61160f4895e0b
taylortackett-basised <41164433+taylortackett-basised@users.noreply.github.com>
2018-07-12 15:03:12 -0700

    Creating New GradeBookTypes
    added RankedGradeBook class
    added StandardGradeBook class

---
+++ GradeBook/StandardGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)                  // adding : base(name) after constructor declaration makes the program execute code in both classes (parent/child)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
