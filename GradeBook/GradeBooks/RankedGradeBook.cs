1aa895d3d7007736a9029b352efdbbd40d14b28f
taylortackett-basised <41164433+taylortackett-basised@users.noreply.github.com>
2018-07-12 19:22:24 -0700

    start here

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -21,7 +21,13 @@ namespace GradeBook.GradeBooks
             
         }
 
-        
+
+        //Ranked-grading grades students not based on their individual performance, but rather their performance compared to their 
+        //peers.This means the 20% of the students with the highest grade of a class get an A, the next highest 20% get a B, etc.
+        //There are many ways to calculate this. I'd recommend figuring out how many students it takes to drop a letter 
+        //grade (20% of the total number of students) X, put all the average grades in order, then figure out where the input 
+        //grade would fit in the series of grades. Every X students with higher grades than the input grade knocks the letter grade 
+        //down until you reach F.
 
 
     }
